3c0a2fd56a0296f3362e98bb394a9a5ee4b91327
Malo Ranzetti <maloranzetti@gmail.com>
2022-03-01 17:09:01 +0100

    db

--- static/terminal.js
+++ static/terminal.js
@@ -58,7 +58,6 @@ $(function() {
 
     $(document).on('keydown', function(e) {
         var c = e.which;
-        alert(e.which);
         //Catch return
         if(e.which == 13) {
             alert('You pressed enter!');
