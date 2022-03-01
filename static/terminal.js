bdd09a0274c0b25073d0f081951771ec7f35afcd
Malo Ranzetti <maloranzetti@gmail.com>
2022-03-01 16:16:53 +0100

    dev

--- static/terminal.js
+++ static/terminal.js
@@ -3,9 +3,17 @@ $(document).on('keypress',function(key) {
     if(key.which == 13) {
         alert('You pressed enter!');
     }
-    if(key.which == 8){
+    if (key.keyCode == $.ui.keyCode.BACKSPACE) {
+
+        // Filters out events coming from any of the following tags so Backspace
+        // will work when typing text, but not take the page back otherwise.
+        var rx = /INPUT|SELECT|TEXTAREA/i;
+        if(!rx.test(e.target.tagName) || e.target.disabled || e.target.readOnly ){
+            e.preventDefault();
+        }
+
         alert('backspace trapped');
-    }
+     }
 
     $('#output').append(document.createTextNode(String.fromCharCode(key.which)));
 });
