19f7da5f866ef227ae867999e9cf553eb1530e7f
Malo Ranzetti <maloranzetti@gmail.com>
2022-03-01 16:52:50 +0100

    dev

--- static/terminal.js
+++ static/terminal.js
@@ -1,11 +1,5 @@
-$(document).on('keydown',function(key) {
-    if(key.which == 13) {
-        alert('You pressed enter!');
-    }
-    else if(key.which == 8){
-        alert('backspace trapped');
-    }
-}).on('keypressed',function(key) {
+
+$('#output').on('keypressed',function(key) {
     //Catch return
     if(key.which == 13) {
         
@@ -13,4 +7,76 @@ $(document).on('keydown',function(key) {
         $('#output').append(document.createTextNode(String.fromCharCode(key.which)));
     }
 
+});
+
+
+$(function() {
+
+    var _to_ascii = {
+        '188': '44',
+        '109': '45',
+        '190': '46',
+        '191': '47',
+        '192': '96',
+        '220': '92',
+        '222': '39',
+        '221': '93',
+        '219': '91',
+        '173': '45',
+        '187': '61', //IE Key codes
+        '186': '59', //IE Key codes
+        '189': '45'  //IE Key codes
+    }
+
+    var shiftUps = {
+        "96": "~",
+        "49": "!",
+        "50": "@",
+        "51": "#",
+        "52": "$",
+        "53": "%",
+        "54": "^",
+        "55": "&",
+        "56": "*",
+        "57": "(",
+        "48": ")",
+        "45": "_",
+        "61": "+",
+        "91": "{",
+        "93": "}",
+        "92": "|",
+        "59": ":",
+        "39": "\"",
+        "44": "<",
+        "46": ">",
+        "47": "?"
+    };
+
+    $(document).on('keydown', function(e) {
+        var c = e.which;
+        //Catch return
+        if(key.which == 13) {
+            alert('You pressed enter!');
+        }
+        else if(key.which == 8){
+            alert('backspace trapped');
+        }
+        else {
+
+            //normalize keyCode 
+            if (_to_ascii.hasOwnProperty(c)) {
+                c = _to_ascii[c];
+            }
+
+            if (!e.shiftKey && (c >= 65 && c <= 90)) {
+                c = String.fromCharCode(c + 32);
+            } else if (e.shiftKey && shiftUps.hasOwnProperty(c)) {
+                //get shifted keyCode value
+                c = shiftUps[c];
+            } else {
+                c = String.fromCharCode(c);
+            }
+            $('#output').append(document.createTextNode(c));
+        }
+    });    
 });
