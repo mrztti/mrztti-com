d96a937380ecff1fdee21e38476ec435a13020b6
Malo Ranzetti <maloranzetti@gmail.com>
2022-03-02 16:08:13 +0100

    db

--- static/terminal.js
+++ static/terminal.js
@@ -13,7 +13,7 @@ var available_commands =
 
 function cmd(c){
     if (c == "help"){
-        return "<div class='warning text-center'> --==-- Available commands: --==--</div> <ul class='list-group list-group-flush faded'> <li class='list-group-item'>" + available_commands.join("</li><li class='list-group-item'>") + "</li></ul>";
+        return "<div class='warning text-center wide'> --==-- Available commands: --==--</div> <ul class='list-group list-group-flush faded'> <li class='list-group-item'>" + available_commands.join("</li><li class='list-group-item'>") + "</li></ul>";
     }
     if (c == "ls"){
         return "You found help"
